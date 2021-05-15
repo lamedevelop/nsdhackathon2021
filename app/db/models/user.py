ec0abe6c4a32cfefc596e135d88e9498d3a1eb64
madzemlya <madzemlya@gmail.com>
2021-05-15 15:45:50 +0300

    update models

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -2,5 +2,15 @@ from pydantic import BaseModel, validator
 
 
 class User(BaseModel):
-    # fields
-    pass
+    user_id: int
+    password_hash: str
+    phone: str
+    email: str
+    tg_id: int
+    tg_authdate: int
+    tg_hash: str
+    first_name: str
+    last_name: str
+    last_auth: int
+    last_update: int
+    registration_date: int
