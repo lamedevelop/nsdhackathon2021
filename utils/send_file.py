fb43433cb759838c15edebc7b170d514c9af2f0a
oleggr <oleg.gr@outlook.com>
2021-05-16 00:38:05 +0300

    utils folder added

--- utils/send_file.py
+++ utils/send_file.py
@@ -1,7 +1,7 @@
 import requests
 
 
-f = open("test.txt", 'rb')
+f = open("../test.txt", 'rb')
 files = {"file": (f.name, f, "multipart/form-data")}
 requests.post(url="http://127.0.0.1:8000/api/send", files=files)
 f.close()
