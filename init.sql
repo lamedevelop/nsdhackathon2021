581f4cb155f1f3b51f99b57ef1e6e4142f4357d3
oleggr <oleg.gr@outlook.com>
2021-05-16 00:02:20 +0300

    insert fixed

--- init.sql
+++ init.sql
@@ -35,25 +35,27 @@ CREATE TABLE tg_notifications (
 );
 
 INSERT INTO
-  `users` (`user_id`, `password_hash`, `phone`, `email`, `tg_id`, `tg_authdate`, `tg_hash`, `first_name`, `last_name`, `last_auth`, `last_update`, `registration_date`)
+  users (`password_hash`, `phone`, `email`, `tg_id`, `tg_authdate`, `tg_hash`, `first_name`, `last_name`, `last_auth`, `last_update`, `registration_date`)
 VALUES
-  (0, '1234', '891045245', 'qq@gmail.com', 122031432, 2, 'some text', 'oleg', 'grinya', 3, 4, 5),
-  (1, '1234', '891045535', 'ww@gmail.com', 297002761, 7, 'another text', 'kolyan', 'shkalka', 8, 9, 10),
-  (2, '1234', '812045245', 'nn@gmail.com', 392009652, 12, 'right text', 'anzor', '???', 13, 14, 15),
-  (3, '1234', '246742188', 'bb@gmail.com', 368497600, 17, 'left text', 'ilya', 'zemlya', 18, 19, 20),
+  ('1234', '891045245', 'qq@gmail.com', 122031432, 2, 'some text', 'oleg', 'grinya', 3, 4, 5),
+  ('1234', '891045535', 'ww@gmail.com', 297002761, 7, 'another text', 'kolyan', 'shkalka', 8, 9, 10),
+  ('1234', '812045245', 'nn@gmail.com', 392009652, 12, 'right text', 'anzor', '???', 13, 14, 15),
+  ('1234', '246742188', 'bb@gmail.com', 368497600, 17, 'left text', 'ilya', 'zemlya', 18, 19, 20);
 
 INSERT INTO
-  `messages` (`sender_id`, `receiver_id`, `message`, `filepath`, `creation_date`, `viewed`)
+  messages (`sender_id`, `receiver_id`, `message`, `filepath`, `creation_date`, `viewed`)
 VALUES
-  (0, 1, 'this is testing', 21, 22, 0),
-  (2, 3, 'this is fake', 23, 24, 0),
-  (1, 2, 'fakefactorio', 25, 26, 0),
-  (2, 3, 'lie', 27, 28, 1),
+  (0, 1, 'this is testing', 'test.txt', 22, 0),
+  (2, 3, 'this is fake', 'test.txt', 24, 0),
+  (1, 2, 'fakefactorio', 'test.txt', 26, 0),
+  (2, 3, 'lie', 'test.txt', 28, 1);
 
 INSERT INTO
-  `tg_notifications ` (`notification_id`, `tg_id`, `message`, `executed`, `execution_date`, `creation_date`)
+  tg_notifications (`tg_id`, `message`, `executed`, `execution_date`, `creation_date`)
 VALUES
-  (0, 'kekmarakek', 'this is testing', 0, 29, 30),
-  (1, 'kekmarakek', 'this is fake', 0, 31, 32),
-  (2, 'kekmarakek', 'fakefactorio', 1, 33, 34),
-  (3, 'kekmarakek', 'lie',1, 35, 36),
+  (122031432, 'this is testing 1', 1, 29, 30),
+  (122031432, 'this is testing 2', 0, 29, 30),
+  (122031432, 'this is testing 3', 0, 29, 30),
+  (297002761, 'this is fake', 0, 31, 32),
+  (392009652, 'fakefactorio', 1, 33, 34),
+  (368497600, 'lie',1, 35, 36);
