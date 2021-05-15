a922883994b166c32a0b4a17925f5f3b2cb96a1b
oleggr <oleg.gr@outlook.com>
2021-05-15 18:10:13 +0300

    message fixes

--- app/db/models/message.py
+++ app/db/models/message.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, validator
 
 
 class Message(BaseModel):
+    message_id: int
     sender_id: int
     receiver_id: int
     message: str
--- app/routes/api/app_router.py
+++ app/routes/api/app_router.py
@@ -2,19 +2,10 @@ from fastapi import APIRouter, Request
 from starlette import status
 from starlette.responses import JSONResponse
 
+from app.db.services.messages import MessagesService
+
 
 router = APIRouter()
-
-
-@router.get(
-    "/hello",
-    name='api:hello',
-    status_code=status.HTTP_200_OK
-)
-async def hello():
-    return JSONResponse(
-        {'response': "HELLO FROM ORKS!"}
-    )
 
 
 @router.get(
@@ -25,4 +16,16 @@ async def hello():
 async def auth():
     return JSONResponse(
         {'response': "HELLO FROM ORKS!"}
+    )
+
+
+@router.get(
+    "/zip_struct",
+    name='api:get-zi-struct',
+    status_code=status.HTTP_200_OK
+)
+async def get_zip_struct(message_id: int):
+    message_service = MessagesService()
+    return JSONResponse(
+        {'data': await message_service.getZipFileStruct(message_id)}
     )
