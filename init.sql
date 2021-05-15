c32bd55fb71f81fbe50b3f204c63ec9f93c86905
madzemlya <51022587+madzemlya@users.noreply.github.com>
2021-05-15 21:19:06 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -34,7 +34,6 @@ CREATE TABLE tg_notifications (
     creation_date int
 );
 
-"""
 INSERT INTO
   `users` (`user_id`, `password_hash`, `phone`, `email`, `tg_id`, `tg_authdate`, `tg_hash`, `first_name`, `last_name`, `last_auth`, `last_update`, `registration_date`)
 VALUES
@@ -42,9 +41,7 @@ VALUES
   (1, '1234', '891045535', 'ww@gmail.com', 297002761, 7, 'another text', 'kolyan', 'shkalka', 8, 9, 10),
   (2, '1234', '812045245', 'nn@gmail.com', 392009652, 12, 'right text', 'anzor', '???', 13, 14, 15),
   (3, '1234', '246742188', 'bb@gmail.com', 368497600, 17, 'left text', 'ilya', 'zemlya', 18, 19, 20),
-"""
 
-"""
 INSERT INTO
   `messages` (`sender_id`, `receiver_id`, `message`, `filepath`, `creation_date`, `viewed`)
 VALUES
@@ -52,9 +49,7 @@ VALUES
   (2, 3, 'this is fake', 23, 24, 0),
   (1, 2, 'fakefactorio', 25, 26, 0),
   (2, 3, 'lie', 27, 28, 1),
-"""
 
-"""
 INSERT INTO
   `tg_notifications ` (`notification_id`, `tg_id`, `message`, `executed`, `execution_date`, `creation_date`)
 VALUES
@@ -62,4 +57,3 @@ VALUES
   (1, 'kekmarakek', 'this is fake', 0, 31, 32),
   (2, 'kekmarakek', 'fakefactorio', 1, 33, 34),
   (3, 'kekmarakek', 'lie',1, 35, 36),
-"""
